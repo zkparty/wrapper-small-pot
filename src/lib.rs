4807f954cb22de7c9ff12c56f09a9b9cc51cb266
Nico Serrano <nnico@LAPTOP-6RMK6J51.localdomain>
2022-09-23 15:42:33 -0500

    lib(feat): contribution & subgroup checks bindings

--- src/lib.rs
+++ src/lib.rs
@@ -1,29 +1,88 @@
-mod utils;
-
-use console_error_panic_hook;
-use wasm_bindgen::prelude::*;
+use eyre::Result;
+use ark_serialize::{Read, Write};
 use std::{fs::File, path::Path};
-use small_powers_of_tau::{srs::SRS, srs::Parameters, keypair::PrivateKey};
+use small_powers_of_tau::sdk::{Transcript, transcript_subgroup_check, update_transcript};
 
-#[wasm_bindgen]
-pub fn contribute(json_arr: Vec<u8>, g1_size: usize, g2_size: usize) {
-    console_error_panic_hook::set_once();
-    println!("Hello, wrapper-small-pot!");
-    // TODO: receive entropy from JS
-    let private_key = PrivateKey::rand(4);
+// TEMPORARY COMMENT:
+// To use functions from the project use crate::package_name
+// Because library compilation is done before binary compilation
+// After binary compilation (in main.rs or similar) you can use use wrapper_small_pot::package_name;
 
-    let parameters = Parameters {
-        num_g1_elements_needed: g1_size,
-        num_g2_elements_needed: g2_size,
-    };
-    // TODO: Vec<u8> is not json_arr type
-    //let mut srs = SRS::deserialise(json_arr, parameters);
-    //TODO: keep copying code from old-geoff/accumulator.rs
+/**
+ * We'll use this function in the cli
+ */
+pub fn contribute_with_file(in_path: &str, out_path: &str, string_seed: &str) -> Result<()> {
+    let json = read_json_file(in_path)?;
+    let result = contribute_with_string(json, string_seed)?;
+    write_json_file(out_path, &result)
+}
+/**
+ * We'll use this function in the wasm
+ */
+pub fn contribute_with_string(json: String, string_seed: &str) -> Result<String> {
+    let seed = string_seed.to_owned();
+    let previous_json = serde_json::from_str(&json).unwrap();
+    // TODO: Transcript serialize is not implemented
+    let previous = serde_json::from_value::<Transcript>(previous_json)?;
+    let post = contribute(previous, seed)?;
+    let post_string = serde_json::to_string(&post)?;
+    Ok(post_string)
+}
+/**
+ * Core function: add participant contribution to transcripts
+ */
+fn contribute(previous: Transcript, seed: String) -> Result<Transcript> {
+    // TODO: create secrets
+    let result = update_transcript(transcript, secrets)?;
+    // TODO: check previous
+    // TODO: perform update
+    Ok(Transcript::default())
 }
 
-pub fn load_json_file(path: &Path) -> Result<String> {
+
+/**
+ * We'll use this function in the cli
+ */
+pub fn check_subgroup_with_file(in_path: &str) -> Result<()> {
+    let json = read_json_file(in_path)?;
+    let result = check_subgroup_with_string(json)?;
+    Ok(println!("user contribution is in file: {:?}", result))
+}
+/**
+ * We'll use this function in the wasm
+ */
+pub fn check_subgroup_with_string(json: String) -> Result<bool> {
+    let json = serde_json::from_str(&json).unwrap();
+    // TODO: Transcript serialize is not implemented
+    let transcript = serde_json::from_value::<Transcript>(json)?;
+    let result = subgroup_check(transcript)?;
+    Ok(result)
+}
+/**
+ * Core function: check participant contribution was included
+ */
+fn subgroup_check(transcript: Transcript) -> Result<bool> {
+    let result = transcript_subgroup_check(transcript);
+    Ok(result)
+}
+// TODO: create update_proof_check functions
+
+
+/**
+ * Util functions
+ */
+fn read_json_file(string_path: &str) -> Result<String> {
+    let path = Path::new(string_path);
     let mut file = File::open(path)?;
-    let mut contents = String::new();
-    file.read_to_string(&mut contents)?;
-    Ok(contents)
+    let mut content = String::new();
+    file.read_to_string(&mut content)?;
+    Ok(content)
+}
+
+fn write_json_file(string_path: &str, content: &str) -> Result<()> {
+    let buf = content.as_bytes();
+    let path = Path::new(string_path);
+    let mut file = File::create(path)?;
+    file.write_all(buf)?;
+    Ok(())
 }
---
+++ src/wasm.rs
@@ -0,0 +1,27 @@
+use std::panic;
+use js_sys::Promise;
+use wasm_bindgen::prelude::wasm_bindgen;
+use wasm_bindgen_rayon::init_thread_pool;
+
+use wrapper_small_pot::{read_json_file, contribute_with_string, check_with_string};
+
+#[wasm_bindgen]
+pub fn init_threads(n: usize) -> Promise {
+    panic::set_hook(Box::new(console_error_panic_hook::hook));
+    init_thread_pool(n)
+}
+
+#[wasm_bindgen]
+pub fn contribute_wasm(input: &str, string_seed: &str) -> String {
+    let result = contribute_with_string(
+        input.to_string(),
+        string_seed.to_string()
+    ).unwrap();
+    return format!("{}", result);
+}
+
+#[wasm_bindgen]
+pub fn subgroup_check_wasm(input: &str, string_seed: &str) -> String {
+    let result = check_subgroup_with_string(input).unwrap();
+    return format!("{}", result);
+}
